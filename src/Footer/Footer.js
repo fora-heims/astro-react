c43788db862bff0c621e24544880b104b9bdd469
fora-heims <foraheims@gmail.com>
2021-11-30 17:15:58 -0800

    refining style and appearance

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,6 +4,6 @@ import React, { Component } from 'react'
 
 export default class Footer extends Component {
   render() {
-    return <footer>My first react.js app created using create-react-app</footer>
+    return <footer>My first react.js app created using create-react-app </footer>
   }
 }
